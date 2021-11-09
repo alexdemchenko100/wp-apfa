cb98e28e7f420a9a799e17d662c446de40aa7775
alexdemchenko <alexdemchenko100@gmail.com>
2021-11-09 12:37:18 -0800

    update google map api key

--- template-parts/block/contact/block.php
+++ template-parts/block/contact/block.php
@@ -75,5 +75,5 @@ $form_id	  	   = get_field( 'choices' );
 		}
 </script>
 
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCB6xN9ffUzyPTlA1iTQRi4dP21TZOu7wo&callback=initMap&v=weekly&channel=2" async ></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyClJB5uMJirXNzb_cFslMeZqL8KWhY7SA4&callback=initMap&v=weekly&channel=2" async ></script>
 </section>
